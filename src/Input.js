3dc7121000f70a270d48631643c78216d0b2d79b
Imrane Abdoul <imraneabdoul@gmail.com>
2020-12-03 23:29:11 +0100

    Complete Input component render tests for success=false

--- src/Input.js
+++ src/Input.js
@@ -3,16 +3,39 @@ import {connect} from "react-redux"
 
 class Input extends Component{
     render(){
+        console.log(this.props.success)
+        const contents =this.props.success?
+        null:
+        (
+            <form className="form-inline">
+                <input 
+                data-test="input-box"
+                className="mb-2 mx-sm-3"
+                type="text"
+                placeholder="Enter guess"
+                />
+                <button 
+                    data-test="submit-button"
+                    className="btn btn-primary mb-2"
+                    type="submit"
+
+                >
+                    Submit
+                </button>
+            </form>
+        )
         return(
-            <div>
-                <button></button>
+            <div data-test="component-input">
+                {contents}
             </div>
         )
     }
 }
 
-const mapStateToProps = (state)=>{
-    return {};
+const mapStateToProps = ({success})=>{
+    return {
+        success
+    };
 }
 
 export default connect(mapStateToProps)(Input);
--- src/Input.test.js
+++ src/Input.test.js
@@ -4,23 +4,31 @@ import {findByAttrib,storeFactory} from "../test/testUtils"
 
 const setup = (initialState={}) =>{
     const store = storeFactory(initialState)
-    const wrapper = shallow(<Input store={store}/>).dive().dive() // pass store in the component, dive go in the one level
+    const wrapper = shallow(<Input store={store}/>).dive().dive() // pass store in the component, dive get inside to the class of component, shallow create a virtual component
     return wrapper
 }
 
 
 describe('render',()=>{
     describe('word has not been guessed',()=>{
+        let wrapper;
+        beforeEach(()=>{
+            const initialState = {success:false}
+            wrapper = setup(initialState)
+        })
         test('render component without error',()=>{
-
+            const component = findByAttrib(wrapper,"component-input")
+            expect(component.length).toBe(1)
         })
 
         test('render input box',()=>{
-
+            const inputBox = findByAttrib(wrapper,"input-box")
+            expect(inputBox.length).toBe(1)
         })
 
-        test('render input box',()=>{
-
+        test('render submit button',()=>{
+            const submitButton = findByAttrib(wrapper,"submit-button")
+            expect(submitButton.length).toBe(1)
         })
 
     })
